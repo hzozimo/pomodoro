95eb6ce4c8ca5019eae71d7a477e5476fc41f7eb
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-10-22 20:07:23 -0300

    css do texto alterado para melhor leitura online

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
           Bem vindos ao Teste Técnico de Henrique Zózimo para a VilaApps!
       </p>
       <br/>
-      <p> 
+      <h5> 
         A <a href='https://brasilescola.uol.com.br/dicas-de-estudo/tecnica-pomodoro-que-e-e-como-funciona.htm' target='_blank' rel="noreferrer">Técnica Pomodoro</a> é um método de gerenciamento de tempo desenvolvido por
         Francesco Cirillo no final dos anos 1980. A técnica usa um cronômetro para dividir o
         trabalho em intervalos, tradicionalmente de 25 minutos de duração, separados por
@@ -19,7 +19,7 @@ const Home = () => {
         Porém, buscando uma maior praticidade para esta página que é uma avaliação técnica, os temporizadores
         estarão em segundos e não em minutos. Ou seja, os 25 minutos de duração
         serão 25 segundos e os 5 minutos de intervalo serão 5 segundos.
-      </p>
+      </h5>
         <Link to="/pomodoro/chronometer">Ligar cronômetro</Link>
     </div>
   )
